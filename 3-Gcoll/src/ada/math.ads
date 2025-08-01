3edf19c82bfda08408df4581ff47d329aa53c957
Rezvee <52466279+RezveeRahman@users.noreply.github.com>
2025-07-31 20:47:39 -0400

    new commits (#4)
    merging in types

---
+++ 3-Gcoll/src/ada/math.ads
@@ -0,0 +1,31 @@
+--------------------------------------------------------------------------
+-- @author:       Rezvee Rahman
+-- @date Created: 07/22/2025
+--
+-- @version: Ada-2012
+--------------------------------------------------------------------------
+
+package Math is
+
+  ------------------------------------------------------------------------
+  -- procedures and functions
+  ------------------------------------------------------------------------
+
+  ------------------------------------------------------------------------
+  -- Overview: These methods are basic mathematical operations for floats.
+  ------------------------------------------------------------------------
+  procedure Add_Two_F (A1 : Float;
+                       A2 : Float);
+
+  procedure Sub_Two_F (A1 : Float;
+                       A2 : Float);
+
+  procedure Div_Two_F (A1 : Float;
+                       A2 : Float);
+
+  procedure Mult_Two_F (A1 : Float;
+                        A2 : Float);
+
+
+end Math;
+
